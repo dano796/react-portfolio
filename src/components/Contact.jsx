144dd3c1748a3808687205c4101915f698ebf6c1
Daniel Ortiz <daniel.ortiza.col@upb.edu.co>
2025-02-16 19:22:43 -0500

    updated mailto

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,13 +15,8 @@ const Contact = () => {
               initial={{opacity: 0, x: -50}}
               transition={{duration: 1.25}}
               className="my-4">{CONTACT.address}</motion.p>
-            <motion.p 
-              whileInView={{opacity: 1, x: 0}}
-              initial={{opacity: 0, x: 50}}
-              transition={{duration: 1.5}}
-              className="my-4">{CONTACT.phoneNo}</motion.p>
             <a 
-              href="mailto:daniel.ortiza.col@upb.edu.co"
+              href="mailto:ortizdaniel796@gmail.com"
               className="border-b">
                 {CONTACT.email}
             </a>
